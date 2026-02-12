ba39600572034c0213633df95ccefed44bb193aa
kaan9898 <kaankarahan98@gmail.com>
2026-02-12 17:40:48 +0200

    Project structure is committed.

---
+++ src/main/java/com/team3/driveza/DrivezaApplication.java
@@ -0,0 +1,13 @@
+package com.team3.driveza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/driveza/DrivezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.driveza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
