295ffc85761fea02094d36e97c3bf90decca8bff
Gayantha Chamith <73245735+gayanthachamith@users.noreply.github.com>
2026-02-16 11:26:01 +0200

    login page
    adding login page ui with the background image

--- src/main/java/com/team3/driveza/config/SecurityConfig.java
+++ src/main/java/com/team3/driveza/config/SecurityConfig.java
@@ -1,4 +1,74 @@
 package com.team3.driveza.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.HttpSecurityBeanDefinitionParser;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                //authorization rule
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                //custom login page
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+
+                //logout
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll()
+                )
+
+                .csrf(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+    //temporary user (later we can replace this with DB based user )
+        @Bean
+                public UserDetailsService userDetailsService(){
+            UserDetails admin = User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("admin")
+                    .roles("ADMIN")
+                    .build();
+
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("user")
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(admin, user);
+        }
+
+
+
+
+    }
+
