6aacb7d9d46088c2e2ebb484ee674b40e7badf99
kaan9898 <kaankarahan98@gmail.com>
2026-02-16 19:59:11 +0200

    CarResponseDto.java is committed.

---
+++ src/main/java/com/team3/driveza/Dto/Car/CarResponseDto.java
@@ -0,0 +1,10 @@
+package com.team3.driveza.Dto.Car;
+
+public class CarResponseDto {
+    private int id;
+    private String brand;
+    private String model;
+    private double pricePerKm;
+    private int fuelLevel;
+    private double distance;
+}
