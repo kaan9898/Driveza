13331cea464aeb26f6246411cb1904671304edf9
kaan9898 <kaankarahan98@gmail.com>
2026-02-16 19:56:27 +0200

    CarRequestDto.java is committed.

---
+++ src/main/java/com/team3/driveza/Dto/Car/CarRequestDto.java
@@ -0,0 +1,11 @@
+package com.team3.driveza.Dto.Car;
+
+public class CarRequestDto {
+    private String model;
+    private String brand;
+    private double pricePerKm;
+    private int fuelLevel;
+    private int latitude;
+    private int longitude;
+    private String status;
+}
