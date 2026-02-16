65927a55e03f0cdb936a3ae9112acad1c373e9cd
kaan9898 <kaankarahan98@gmail.com>
2026-02-16 20:05:12 +0200

    AdminCarResponseDto.java is committed.

---
+++ src/main/java/com/team3/driveza/Dto/Car/AdminCarResponseDto.java
@@ -0,0 +1,15 @@
+package com.team3.driveza.Dto.Car;
+
+public class AdminCarResponseDto {
+    private int id;
+    private String brand;
+    private String model;
+    private enum statusEnum {
+        ACTIVE, INACTIVE
+    }
+    private statusEnum status;
+    private double pricePerKm;
+    private int fuelLevel;
+    private int latitude;
+    private int longitude;
+}
