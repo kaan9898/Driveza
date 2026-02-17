285ea7bcf59dfa9b726b4bec71bbcd8671d6eb55
kaan9898 <kaankarahan98@gmail.com>
2026-02-17 13:41:38 +0200

    UserDetailsImpl.java is committed.

---
+++ src/main/java/com/team3/driveza/service/UserDetailsImpl.java
@@ -0,0 +1,38 @@
+package com.team3.driveza.service;
+
+import com.team3.driveza.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+    @Override public boolean isAccountNonExpired()     { return true; }
+    @Override public boolean isAccountNonLocked()      { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled()               { return true; }
+    public User getUser() {
+        return user;
+    }
+}
