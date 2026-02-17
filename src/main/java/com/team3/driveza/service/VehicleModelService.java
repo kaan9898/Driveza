a96e4cd2a3cdca1c3c71538265bd7f923c6b54ff
Diamondtroller <46786192+Diamondtroller@users.noreply.github.com>
2026-02-17 14:10:50 +0200

    service: vehicle model service

--- src/main/java/com/team3/driveza/service/VehicleModelService.java
+++ src/main/java/com/team3/driveza/service/VehicleModelService.java
@@ -2,35 +2,51 @@ package com.team3.driveza.service;
 
 
 import com.team3.driveza.model.VehicleModel;
-import com.team3.driveza.repository.VehicleRepository;
+import com.team3.driveza.repository.VehicleModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleModelService {
     @Autowired
-    private VehicleRepository vehicleRepository;
+    private VehicleModelRepository vehicleModelRepository;
 
+    // TODO: Use DTOs
     public List<VehicleModel> getAllModels() {
-        return new ArrayList<>();
+        return vehicleModelRepository.findAll();
     }
 
-    public VehicleModel getModelById(Long id) {
-        return new VehicleModel();
+    public VehicleModel getModelById(long id) throws RuntimeException {
+        return getModelWithoutDTO(id);
     }
 
-    public VehicleModel createModel(VehicleModel model) {
-        return model;
+    public VehicleModel createModel(VehicleModel newVehicleModel) {
+        VehicleModel vehicleModel = new VehicleModel();
+        vehicleModel.setModel(newVehicleModel.getModel());
+        vehicleModel.setBrand(newVehicleModel.getBrand());
+        return vehicleModel;
     }
 
-    public VehicleModel updateModel(Long id, VehicleModel model) {
-        return model;
+    public VehicleModel updateModel(long id, VehicleModel newVehicleModel) throws RuntimeException {
+        VehicleModel vehicleModel = getModelWithoutDTO(id);
+        vehicleModel.setModel(newVehicleModel.getModel());
+        vehicleModel.setBrand(newVehicleModel.getBrand());
+        return vehicleModel;
     }
 
-    public void deleteModel(Long id) {
-        // nothing for now
+    public void deleteModel(long id) {
+        VehicleModel vehicleModel = getModelWithoutDTO(id);
+        vehicleModelRepository.delete(vehicleModel);
+    }
+
+    public VehicleModel getModelWithoutDTO(long id) throws RuntimeException {
+        Optional<VehicleModel> optionalVehicleModel = vehicleModelRepository.findById(id);
+        if (optionalVehicleModel.isEmpty()) {
+            throw new RuntimeException("This vehicle model does not exist.");
+        }
+        return optionalVehicleModel.get();
     }
 }
