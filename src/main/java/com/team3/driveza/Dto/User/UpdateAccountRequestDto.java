6f2eb9d47b732c2b35ce4f167fd9a80e8a1a0ab1
akicds <halilberk.aki.dev@gmail.com>
2026-02-18 14:12:12 +0200

    Account request update dto class is updated

--- src/main/java/com/team3/driveza/Dto/User/UpdateAccountRequestDto.java
+++ src/main/java/com/team3/driveza/Dto/User/UpdateAccountRequestDto.java
@@ -1,7 +1,25 @@
 package com.team3.driveza.Dto.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateAccountRequestDto {
-    private String username;
+    @NotBlank(message = "Name is required.")
+    private String name;
+
+    @Email(message = "Must be a valid email address.")
+    @NotBlank(message = "Email is required.")
     private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters.")
     private String password;
 }
