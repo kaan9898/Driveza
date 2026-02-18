b75e85defd60a5c0009410c2954f7fb6c0675077
akicds <halilberk.aki.dev@gmail.com>
2026-02-18 14:04:00 +0200

    AccountResponseDto class is updated

--- src/main/java/com/team3/driveza/Dto/User/AccountResponseDto.java
+++ src/main/java/com/team3/driveza/Dto/User/AccountResponseDto.java
@@ -1,8 +1,17 @@
 package com.team3.driveza.Dto.User;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountResponseDto {
-    private String role;
-    private int id;
-    private String username;
+    private Long id;
+    private String name;
     private String email;
+    private String role;
 }
