a4307c2f0774b5117ea6a60bd0fe7a0024b26552
kaan9898 <kaankarahan98@gmail.com>
2026-02-18 11:50:56 +0200

    Configured postgresql's correct table name

--- src/main/java/com/team3/driveza/model/User.java
+++ src/main/java/com/team3/driveza/model/User.java
@@ -1,29 +1,39 @@
 package com.team3.driveza.model;
 
+import com.team3.driveza.model.User;
 import com.team3.driveza.model.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.time.ZonedDateTime;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 public class User {
+    @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @NotBlank
     private String name;
+
     @NotBlank
     private String password;
+
     @NotBlank
     private String email;
+
     @Past
     private ZonedDateTime dob;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 }
