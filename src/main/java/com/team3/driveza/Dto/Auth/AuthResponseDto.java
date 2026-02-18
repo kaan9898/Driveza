7cc0ee8d090a5d63ce110f411d97be692405c692
akicds <halilberk.aki.dev@gmail.com>
2026-02-18 14:04:25 +0200

    AuthtResponseDto class is updated

--- src/main/java/com/team3/driveza/Dto/Auth/AuthResponseDto.java
+++ src/main/java/com/team3/driveza/Dto/Auth/AuthResponseDto.java
@@ -1,7 +1,19 @@
 package com.team3.driveza.Dto.Auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponseDto {
-    private String token;
-    private String username;
+
+    private Long id;
+    private String name;
+    private String email;
     private String role;
+    private String message;
 }
