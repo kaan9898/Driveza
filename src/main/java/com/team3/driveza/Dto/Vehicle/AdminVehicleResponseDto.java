46c3c0259c5093b4b7b0d5dbb381ac097398fb88
akicds <halilberk.aki.dev@gmail.com>
2026-02-19 11:30:08 +0200

    Add AdminVehicleResponseDto for admin payloads

--- src/main/java/com/team3/driveza/Dto/Vehicle/AdminVehicleResponseDto.java
+++ src/main/java/com/team3/driveza/Dto/Vehicle/AdminVehicleResponseDto.java
@@ -1,15 +1,23 @@
 package com.team3.driveza.Dto.Vehicle;
 
+import com.team3.driveza.model.enums.VehicleStatus;
+import com.team3.driveza.model.enums.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminVehicleResponseDto {
-    private int carId;
+    private Long id;
     private String brand;
-    private String model;
-    private enum statusEnum {
-        ACTIVE, INACTIVE
-    }
-    private statusEnum status;
-    private double pricePerKm;
-    private int fuelLevel;
-    private int latitude;
-    private int longitude;
+    private String modelName;
+    private VehicleStatus status;
+    private VehicleType type;
+    private double pricePerMin;
+    private double latitude;
+    private double longitude;
 }
