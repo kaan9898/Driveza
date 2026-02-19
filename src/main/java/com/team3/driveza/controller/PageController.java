c5a5b8a91dee748e5837f7a26188ced4bc6eaea0
Gayantha Chamith <73245735+gayanthachamith@users.noreply.github.com>
2026-02-19 09:34:46 +0200

    Task 10
    adding active vehicle location showing part in map and UI optimization

--- src/main/java/com/team3/driveza/controller/PageController.java
+++ src/main/java/com/team3/driveza/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.team3.driveza.controller;
 
 
+import com.team3.driveza.model.Vehicle;
 import com.team3.driveza.service.UserService;
 import com.team3.driveza.service.VehicleService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,11 +40,34 @@ public class PageController {
         return "account";
     }
 
-
+//loading map to ui and showing available vehicle location point in map
     @GetMapping("/map")
-    public String map(Model model){
-         model.addAttribute("cars",vehicleService.getAvailableVehicles());
+    public String map(@RequestParam(required = false) Double lat, @RequestParam(required = false) Double lon,
+    @RequestParam(required = false) Double radius,
+            Model model ){
+
+        List<Vehicle> vehicleList;
+        if (lat == null || lon == null) {
+
+        vehicleList =vehicleService.getAvailableVehicles();
+        }else{
+            vehicleList = vehicleService.getNearbyVehicles(lat,lon,radius);
+
+            if(vehicleList.isEmpty()){
+                vehicleList = vehicleService.getAvailableVehicles();
+            }
+        }
+        System.out.println("MAP vehicle count : "+ vehicleList.size());
+        model.addAttribute("vehicles", vehicleList);
+        model.addAttribute("lat", lat);
+        model.addAttribute("lon", lon);
+        model.addAttribute("radius", radius);
+//        model.addAttribute("cars", vehicleService.getAvailableVehicles());
         return "map";
+//        }
+//         System.out.println("available vehicle" + vehicleService.getAvailableVehicles().size());
+//        System.out.println("nearby vehicle" + vehicleService.getNearbyVehicles(lat, lon, radious).size());
+//        return "map";
     }
 
 }
