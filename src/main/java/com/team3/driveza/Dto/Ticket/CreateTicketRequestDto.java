c4d885dc591a4f047e18a833afd437317d63093c
kaan9898 <kaankarahan98@gmail.com>
2026-02-24 13:51:39 +0200

    CreateTicketRequestDto.java is updated.

--- src/main/java/com/team3/driveza/Dto/Ticket/CreateTicketRequestDto.java
+++ src/main/java/com/team3/driveza/Dto/Ticket/CreateTicketRequestDto.java
@@ -1,5 +1,10 @@
 package com.team3.driveza.Dto.Ticket;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateTicketRequestDto {
     private String title;
     private String description;
